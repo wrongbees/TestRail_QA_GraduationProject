901e123512abc0ad9de7822e5d135b4c50b1e75c
ntshs <ntshsttxm1997@gmail.com>
2021-09-04 18:29:25 +0300

    Bla Bla

---
+++ src/test/java/model/Model.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.testng.annotations.Test;
+
+public class Model {
+
+    @Test
+    public void second(){
+
+    }
+}
---
+++ src/test/java/tests/TestTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestTest {
+
+    @Test
+    public void first() {
+    }
+
+
+}
