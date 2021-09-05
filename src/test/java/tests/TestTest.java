7043ef17a43b012bd86b40fcf1908aad14ae6763
ntshs <ntshsttxm1997@gmail.com>
2021-09-05 11:16:07 +0300

    Немного почистила проект, добавила один простой unit тест, подключила аллюр

--- src/test/java/tests/TestTest.java
+++ src/test/java/tests/TestTest.java
@@ -1,11 +1,16 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestTest {
 
     @Test
     public void first() {
+        int a = 4;
+        int b = 2;
+        int result = a/b;
+        Assert.assertEquals(result,2);
     }
 
 
