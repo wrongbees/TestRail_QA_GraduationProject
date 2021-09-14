7e8639d332a02301c9b54696c5e4d7372b4d2ddb
Vladimir <mkkinj@gmail.com>
2021-09-14 21:47:45 +0300

    Поковыряли робота

--- src/test/java/executors/RobotExecutor.java
+++ src/test/java/executors/RobotExecutor.java
@@ -4,30 +4,32 @@ import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.io.File;
-import java.util.Objects;
+
 
 public class RobotExecutor {
 
-
     public static void downloadFile(String fileName) throws AWTException {
         System.out.println("******************Робот включился*****************");
-        Robot robot = new Robot();;
-        ClassLoader classLoader;
-        File file;
-        StringSelection stringSelection;
+        Robot robot = new Robot();
+//        ClassLoader classLoader;
+//        File file;
+        //StringSelection stringSelection;
 
 
-        classLoader = RobotExecutor.class.getClassLoader();
+        //classLoader = RobotExecutor.class.getClassLoader();
 
-        file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
+       // file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
 
-        String absolutePath = file.getAbsolutePath();
+      //  String absolutePath = file.getAbsolutePath();
 
-        stringSelection = new StringSelection(absolutePath);
+        //stringSelection = new StringSelection(absolutePath);
 
+        //Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+
+        robot.delay(3000);
+        StringSelection stringSelection = new StringSelection( new File(fileName).getAbsolutePath());
         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
 
-        robot.setAutoDelay(1000);
         robot.keyPress(KeyEvent.VK_CONTROL);
         robot.keyPress(KeyEvent.VK_V);
 
@@ -40,3 +42,17 @@ public class RobotExecutor {
         System.out.println("******************робот выключился*****************");
     }
 }
+//    Robot robot = new Robot();
+//        robot.delay(3000);
+//                StringSelection stringSelection = new StringSelection(getFile());
+//                Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+//
+//                robot.keyPress(KeyEvent.VK_CONTROL);
+//                robot.keyPress(KeyEvent.VK_V);
+//                robot.keyRelease(KeyEvent.VK_V);
+//                robot.keyRelease(KeyEvent.VK_CONTROL);
+//                robot.keyPress(KeyEvent.VK_ENTER);
+//                robot.keyRelease(KeyEvent.VK_ENTER);
+//                String getFile() {
+//                return new File("Man-Silhouette.jpg").getAbsolutePath();
+//                }
